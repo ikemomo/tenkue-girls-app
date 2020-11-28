c04fae10bfbcbe90e1bf6dd76d2c5aad6256f7a7
yell <ginger.yell.1209@gmail.com>
2020-11-29 03:05:24 +0900

    postsコントローラにcreateアクションを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationControllers
+  
   def new
     @post = Post.new
+  end
+
+  def create
+    # @post = Post.new(post_params)
+    # if @post.save
+    #   redirect_to user_path
+    # else
+    #   render :new
+    # end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:start_date, :end_date, :marriage, :child, :childcare, :care, :employment_status, :content).merge(user_id: current_user.id)
   end
 end
