080abec66a7c7ef82df840e900e07f066b4dd20e
yell <ginger.yell.1209@gmail.com>
2020-11-29 12:36:09 +0900

    postsコントローラのcreateアクションを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,21 @@
-class PostsController < ApplicationControllers
-  
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @post = Post.new
   end
 
   def create
-    # @post = Post.new(post_params)
-    # if @post.save
-    #   redirect_to user_path
-    # else
-    #   render :new
-    # end
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to user_path(@post.user.id)
+    else
+      render :new
+    end
   end
 
   private
   def post_params
-    params.require(:post).permit(:start_date, :end_date, :marriage, :child, :childcare, :care, :employment_status, :content).merge(user_id: current_user.id)
+    params.require(:post).permit(:start_at, :end_at, :marriage, :child, :childcare, :care, :employment_status, :content).merge(user_id: current_user.id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :employment_status, presence: true, acceptance: { accept: ['正社員', '契約社員','業務委託', 'フリーランス', 'アルバイト', 'その他'] }
+  validates :marriage, presence: true, acceptance: { accept: ['未婚', '既婚', '離別'] }
+  validates :childcare, presence: true, acceptance: { accept: ['子供なし', '子供あり(育児なし)', '子供あり(育児あり)'] }
+  validates :care, presence: true
 end
