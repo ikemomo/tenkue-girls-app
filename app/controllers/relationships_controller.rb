d1bde936ab5aec217ec08896fe497f45bac65c83
yell <ginger.yell.1209@gmail.com>
2020-11-30 21:32:02 +0900

    relationshipasコントローラにcreateアクションを追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,8 @@
 class RelationshipsController < ApplicationController
   def create
+    follow = current_user.active_relationships.build(follower_id: params[:user_id])
+    follow.save
+    redirect_back fallback_location: @user
   end
 
   def destroy
