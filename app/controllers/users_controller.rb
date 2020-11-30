778225a6d3f4b81d5939b02c13f09c6bb580196f
yell <ginger.yell.1209@gmail.com>
2020-11-30 22:16:52 +0900

    userコントローラにfollowerアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :follow, :follower]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
     @post = @user.posts.last
     @posts = @user.posts.order("created_at DESC")
   end
 
   def follow
-    @user = User.find(params[:id])
     @followings = @user.followings.order
+  end
+
+  def follower
+    @followers = @user.followers.order
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
