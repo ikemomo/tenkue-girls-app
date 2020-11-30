43ac0152801d4a8da3ff851066f941cd919c655b
yell <ginger.yell.1209@gmail.com>
2020-11-30 20:16:43 +0900

    relationshipsのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    resource :relationships, only: [:create, :destroy]
   end
   root to: "home#index"
   get 'home/show'
