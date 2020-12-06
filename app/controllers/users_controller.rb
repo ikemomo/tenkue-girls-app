a77a52aa96b46af3f613dd6198b08c24161c5c79
yell <ginger.yell.1209@gmail.com>
2020-12-06 11:45:04 +0900

    ユーザ一覧を作成順に並び替えるように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :follow, :follower]
 
   def index
-    @q = User.ransack(params[:q])
+    @q = User.order(created_at: :desc).ransack(params[:q])
     @users = @q.result(distinct: true)
   end
 
