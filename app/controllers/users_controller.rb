e99678d490eeb739d3ff7798cf79a0349aba9203
yell <ginger.yell.1209@gmail.com>
2020-12-08 22:03:49 +0900

    誕生日、エンジニア開始日が登録されているかの条件を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,20 @@ class UsersController < ApplicationController
 
   def show
     @post = @user.posts.last
-    @user.update(
-      age: (Date.today.strftime('%Y%m%d').to_i - @user.birthday.strftime('%Y%m%d').to_i) / 10000,
-      engineer_history: (Date.today.strftime('%Y%m%d').to_i - @user.engineer_start_at.strftime('%Y%m%d').to_i) / 10000,
-      marriage: @post.marriage,
-      childcare: @post.childcare,
-      care: @post.care,
-      employment_status: @post.employment_status
-    )
+    unless @user.birthday.nil?
+      @user.age = (Date.today.strftime('%Y%m%d').to_i - @user.birthday.strftime('%Y%m%d').to_i) / 10000
+    end
+    unless @user.engineer_start_at.nil?
+      @user.engineer_history = (Date.today.strftime('%Y%m%d').to_i - @user.engineer_start_at.strftime('%Y%m%d').to_i) / 10000
+    end
+    if @post
+      @user.update(
+        marriage: @post.marriage,
+        childcare: @post.childcare,
+        care: @post.care,
+        employment_status: @post.employment_status
+      )
+    end
     @posts = @user.posts.order("created_at DESC")
   end
 
