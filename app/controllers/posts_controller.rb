d8dc59dd6c0268d83408e9a56c98fd9b47b4f43f
mihomatsui <mihomatsui.24@gmail.com>
2020-12-12 00:32:15 +0900

    postコントローラにprivateメソッドを設置、条件分岐の書き方に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,27 +15,33 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    set_post
   end
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update
+    set_post
+    if @post.update(post_params)
       redirect_to user_path(current_user.id), notice: "投稿を編集しました。"
     else
-      flash.now[:alert] = "投稿編集に失敗しました。"
       render :edit
     end
   end
 
   def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
+    set_post
+    if @post.destroy
       redirect_to user_path(current_user.id),notice: "投稿を削除しました。"
+    else
+      render :edit
+    end
   end
 
   private
   def post_params
     params.require(:post).permit(:start_at, :end_at, :marriage, :child, :childcare, :care, :employment_status, :content).merge(user_id: current_user.id)
+  end
+  private
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
