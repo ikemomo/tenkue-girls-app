f6cd69de666739b13ca0e3679fd5d8fc037e2a8c
mihomatsui <mihomatsui.24@gmail.com>
2020-12-11 18:05:21 +0900

    postコントローラのdestroyアクションを編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,20 +30,12 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
-    if @post.destroy
+    @post.destroy
       redirect_to user_path(current_user.id),notice: "投稿を削除しました。"
-    else
-       flash.now[:alert] = "投稿削除に失敗しました。"
-       render :destroy
-    end
   end
 
   private
   def post_params
     params.require(:post).permit(:start_at, :end_at, :marriage, :child, :childcare, :care, :employment_status, :content).merge(user_id: current_user.id)
-  end
-
-  private
-  def set_post
   end
 end
